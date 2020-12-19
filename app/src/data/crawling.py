ea1d99c9e389417219888745bb89ab0e513d3377
시혁 박 <exon.indiv@gmail.com>
2020-12-19 14:52:14 +0900

    log

--- app/src/data/crawling.py
+++ app/src/data/crawling.py
@@ -14,7 +14,9 @@ def jsonFor(o):
     return string[:-1]
 
 def task():
+    print("[Start]")
     os.system("git pull origin main")
+    print("Start Crawling..")
     webpage = requests.get("http://ncov.mohw.go.kr/")
     soup = BeautifulSoup(webpage.content, "html.parser")
 
@@ -41,8 +43,10 @@ def task():
         }}""")
     f.close()
 
+    print("End Crawling")
     os.system("git commit -am \"update\"")
     os.system("git push origin main")
+    print("[End]")
 
 while True:
     task()
