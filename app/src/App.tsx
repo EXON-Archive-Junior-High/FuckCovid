a74b882e0fdb62dff030c12db9eb66195b5cc99f
시혁 박 <exon.indiv@gmail.com>
2020-12-20 18:38:00 +0900

    fix

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -12,13 +12,13 @@ export default class App extends Component {
                     <h1>{data.all_confirmed_person}</h1>
                     <p className="info">일일 확진자</p>
                     <h1>{data.today_confirmed_person}</h1>
-                    <p>누적 입원 환자</p>
-                    <h2>{data.all_quarantine_release}</h2>
-                    <p>일일 입원 환자</p>
-                    <h2>{data.today_quarantine_release}</h2>
                     <p>누적 격리해제</p>
-                    <h2>{data.all_cure}</h2>
+                    <h2>{data.all_quarantine_release}</h2>
                     <p>일일 격리해제</p>
+                    <h2>{data.today_quarantine_release}</h2>
+                    <p>누적 치료 중</p>
+                    <h2>{data.all_cure}</h2>
+                    <p>일일 치료 중</p>
                     <h2>{data.today_cure}</h2>
                     <p>누적 사망자</p>
                     <h2>{data.all_die}</h2>
